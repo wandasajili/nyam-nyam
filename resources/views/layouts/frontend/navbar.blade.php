3d29e790dfea58bc390dcee210a705a3cec20d6d
aardni <arbianindra@gmail.com>
2025-05-06 00:03:04 +0700

    memindah html ke laravel

---
+++ resources/views/layouts/frontend/navbar.blade.php
@@ -0,0 +1,41 @@
+<div class="container-xxl position-relative p-0">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+        <a href="{{ url('/') }}" class="navbar-brand p-0">
+            <h1 class="text-primary m-0">
+                <img src="{{ asset('img/nyum.png') }}" alt="Logo" style="height: 40px;">
+                NYAM NYAM
+            </h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="fa fa-bars"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0 pe-4">
+                <a href="{{ url('/') }}" class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}">Home</a>
+                <a href="{{ url('/about') }}" class="nav-item nav-link {{ Request::is('about') ? 'active' : '' }}">About</a>
+                <a href="{{ url('/menu') }}" class="nav-item nav-link {{ Request::is('menu') ? 'active' : '' }}">Menu</a>
+                <a href="{{ url('/contact') }}" class="nav-item nav-link {{ Request::is('contact') ? 'active' : '' }}">Contact</a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Dynamic Hero Section -->
+    @hasSection('page-title')
+    <div class="container-xxl py-5 bg-dark hero-header mb-5">
+        <div class="container text-center my-5 pt-5 pb-4">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">
+                @yield('page-title')
+            </h1>
+            
+            @hasSection('breadcrumb')
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb justify-content-center text-uppercase">
+                    @yield('breadcrumb')
+                </ol>
+            </nav>
+            @endif
+        </div>
+    </div>
+    @endif
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
 
-Route::get('/index', function () {
-    return view('index');
+Route::get('/about', function () {
+    return view('home.about');
+});
+
+Route::get('/contact', function () {
+    return view('home.contact');
+});
+
+Route::get('/menu', function () {
+    return view('product.index');
 });
 
 
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-
+?>
 
